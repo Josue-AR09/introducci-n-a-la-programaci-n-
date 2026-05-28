a0628ca9f221642fa85c8c24125f84526f15cc33
Josue-AR09 <arcebrenesjosuedavid@gmail.com>
2026-05-27 20:56:29 -0600

    Add files via upload

---
+++ practica evaluada/src/practica/evaluada/PracticaEvaluada.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.evaluada;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author arceb
+ */
+public class PracticaEvaluada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        String nombre = "ingrese su nombre";
+        String apellido = "ingrese su apellido";
+        String salario = "ingrese su salario individual";
+        String lectura = "ingrese su salario individual";
+        nombre = JOptionPane.showInputDialog(nombre);
+        apellido = JOptionPane.showInputDialog(apellido);
+        salario = JOptionPane.showInputDialog(salario);
+        
+
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+    }
+    
+}
